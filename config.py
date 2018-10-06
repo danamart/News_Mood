8a36cabd59f1a6ae06893e6cb66940c5744a8702
danamart <42128908+danamart@users.noreply.github.com>
2018-10-05 20:31:41 -0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+#  Add your API key
+api_key = "8df728f0fab64600b4e4f132db1a4617"
+owm_api_key = "931e76e2657e7d5df1fa1d29da3108da"
+gkey = "AIzaSyC8LpU3cNG2Jx89DdNUwLZNMJpeTjCIfhE"
+census_key = "85ac64b6b5a9c0901b00329d1ef41f0c53ccfc98"
+consumer_key = "wKfyuNqjipwwtMd2pTQZkpKvE"
+consumer_secret = "E33NLazBf5inkhewfuBAd9hkXLjMCZMx7i3zYH0DUYNGoITr2t"
+access_token = "1039730128308318208-IhbwxtbQeNnZU0gTakUxI4Z3up9Qp2"
+access_token_secret = "BHhusBtjFVliYqsUFFBxUb1ztVsa9EH2LijVF0BDO2HD2"
